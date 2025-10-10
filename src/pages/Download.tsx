4c5065393737044671d8fb3fe5d3a04d5c2e4703
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2025-10-10 21:03:48 +0800

    Update Download.tsx

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -8,7 +8,7 @@ export function Download() {
   const randomUrls = [
     "https://obqj2.com/4/9587058",
     "https://enviousgarbage.com/HE9TFh",
-    "https://meowadvertising.com/hc70ax5ct2?key=7df760c08ecfe3653c332fbdce13d42a",
+    "https://viikqoye.com/dc/?blockID=388556",
     "https://superficial-work.com/ba3RV.0YPk3Xp/v/b/mOVsJHZqDV0Y0KO/DVQWzkOvD/MK3pLvT/QJ4JNmDyM/4MMozHgS"
   ];
 
