69d28b94260e7712a935f862a60d2e3e415ce923
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2025-10-14 21:34:26 +0800

    Update Download.tsx

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -6,10 +6,10 @@ export function Download() {
 
   // Array of URLs for pop-under links
   const randomUrls = [
-    "https://obqj2.com/4/9587058",
-    "https://enviousgarbage.com/HE9TFh",
-    "https://viikqoye.com/dc/?blockID=388556",
-    "https://superficial-work.com/ba3RV.0YPk3Xp/v/b/mOVsJHZqDV0Y0KO/DVQWzkOvD/MK3pLvT/QJ4JNmDyM/4MMozHgS"
+    'https://enviousgarbage.com/HE9TFh',
+    'https://mo.gatsbykynurin.com/iDo8P9Ad0aP/94691',
+    'https://aviatorreproducesauciness.com/2082665',
+    'https://viidedss.com/dc/?blockID=388556'
   ];
 
   const handleDownload = () => {
